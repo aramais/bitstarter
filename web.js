70a396c5c2cc7a8ec9f199ad622bbdd46d95f7c4
Aramais Malkhasyan <aram.malkhasyan@gmail.com>
2013-07-13 19:38:26 +0400

    web.js updated

--- web.js
+++ web.js
@@ -8,8 +8,8 @@ app.get('/', function(request, response) {response.send('pizda rulyu');});
 
 
 var buffer = new buffer(18);
-buffer = fs.readFileSync(index.html);
-buffer.toString('utf-8');
+//buffer = fs.readFileSync(index.html);
+//buffer.toString('utf-8');
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
