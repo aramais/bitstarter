3e002121040fc99179a0ca1487b83a8b4bae096b
Aramais Malkhasyan <aram.malkhasyan@gmail.com>
2013-07-13 17:26:51 +0400

    Updated web.js

--- web.js
+++ web.js
@@ -6,6 +6,17 @@ app.get('/', function(request, response) {
   response.send('Hello World2!');
 });
 
+
+var buffer = new buffer(18);
+buffer = fs.readFileSync(index.html);
+buffer.toString('utf-8');
+
+
+
+
+
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
