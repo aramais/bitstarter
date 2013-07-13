9d35b262e0c47c1782af5477db050cc21ee42180
Aramais Malkhasyan <aram.malkhasyan@gmail.com>
2013-07-13 20:11:57 +0400

    web.js updated

--- web.js
+++ web.js
@@ -4,15 +4,10 @@ var fs = require('fs');
 var express = require('express');
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {response.send('pizda rulyu');});
+var data = fs.readFileSync('index.html', 'utf-8');
 
-var buffer = new Buffer(18);
-fs.readFileSync('index.html',buffer);
-buffer.toString('utf-8');
-
+app.get('/', function(request, response) {response.send(data);});
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-var k = 20;
